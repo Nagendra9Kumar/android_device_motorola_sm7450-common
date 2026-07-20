56aea066b0c6de1877b2e54b3f21c2910cdc1d45
Nagendra <nagendragubbala999+9@gmail.com>
2026-07-20 12:19:34 +0530

    chore: update kernel modules list and BoardConfigCommon.mk configuration

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,115 +79,125 @@ BOARD_BOOTCONFIG += \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3
 
-# Inline Kernel Building (Commented out for future source building)
-# BOARD_KERNEL_IMAGE_NAME := Image
-# BOARD_KERNEL_PAGESIZE := 4096
-# BOARD_USES_GENERIC_KERNEL_IMAGE := true
-# TARGET_KERNEL_SOURCE := kernel/motorola/sm7450
-# TARGET_KERNEL_CONFIG := \
-#     gki_defconfig \
-#     vendor/waipio_GKI.config \
-#     vendor/ext_config/moto-waipio.config \
-#     vendor/ext_config/moto-waipio-gki.config
-# 
-# TARGET_KERNEL_EXT_MODULE_ROOT := kernel/motorola/sm7450-modules
-# 
-# # Kernel Modules
-# BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
-# BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(TARGET_KERNEL_SOURCE)/modules.vendor_blocklist.msm.waipio
-# BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.vendor_boot))
-# BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE)
-# BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery))
-# BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
-# 
-# TARGET_KERNEL_EXT_MODULES := \
-#     qcom/opensource/mmrm-driver \
-#     qcom/opensource/audio-kernel \
-#     qcom/opensource/camera-kernel \
-#     qcom/opensource/cvp-kernel \
-#     qcom/opensource/dataipa/drivers/platform/msm \
-#     qcom/opensource/datarmnet/core \
-#     qcom/opensource/datarmnet-ext/aps \
-#     qcom/opensource/datarmnet-ext/offload \
-#     qcom/opensource/datarmnet-ext/shs \
-#     qcom/opensource/datarmnet-ext/perf \
-#     qcom/opensource/datarmnet-ext/perf_tether \
-#     qcom/opensource/datarmnet-ext/sch \
-#     qcom/opensource/datarmnet-ext/wlan \
-#     qcom/opensource/display-drivers/msm \
-#     qcom/opensource/eva-kernel \
-#     qcom/opensource/video-driver \
-#     qcom/opensource/wlan/qcacld-3.0/.adrastea \
-#     qcom/opensource/wlan/qcacld-3.0/.qca6750
-# 
-# TARGET_KERNEL_EXT_MODULES += \
-#     motorola/drivers/mmi_annotate \
-#     motorola/drivers/mmi_info \
-#     motorola/drivers/backlight/aw99703 \
-#     motorola/drivers/backlight/ktd3136 \
-#     motorola/drivers/power/bm_adsp_ulog \
-#     motorola/drivers/power/mmi_charger \
-#     motorola/drivers/power/qti_glink_charger \
-#     motorola/drivers/power/qpnp_adaptive_charge \
-#     motorola/drivers/power/bq27426_fg_mmi \
-#     motorola/drivers/power/sgm4154x_charger_lite \
-#     motorola/drivers/misc/utag \
-#     motorola/drivers/misc/mmi_stow \
-#     motorola/drivers/mmi_relay \
-#     motorola/drivers/misc/mmi_sys_temp \
-#     motorola/drivers/regulator/dio8015 \
-#     motorola/drivers/regulator/wl2864c \
-#     motorola/drivers/regulator/wl2866d \
-#     motorola/drivers/regulator/slg5bm43670 \
-#     motorola/drivers/sensors \
-#     motorola/drivers/misc/awinic/sarsensor \
-#     motorola/drivers/misc/stk501xx \
-#     motorola/drivers/misc/sx937x \
-#     motorola/drivers/input/touchscreen/touchscreen_mmi \
-#     motorola/drivers/input/touchscreen/focaltech_0flash_v2_mmi \
-#     motorola/drivers/input/touchscreen/ili9882_mmi \
-#     motorola/drivers/input/touchscreen/goodix_berlin_mmi \
-#     motorola/drivers/input/misc/anc_fps_mmi \
-#     motorola/drivers/input/misc/fpc_fps_mmi \
-#     motorola/drivers/input/misc/goodix_fod_mmi \
-#     motorola/drivers/moto_netopt/con_dfpar \
-#     motorola/drivers/nfc/st21nfc \
-#     motorola/drivers/wlan_antenna
-
-# Prebuilt Kernel Configs
+# Inline Kernel Building
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_SEPARATED_DTBO := true
-BOARD_RAMDISK_USE_LZ4 := true
-TARGET_KERNEL_NO_GCC := true
-TARGET_KERNEL_SOURCE := $(DEVICE_PATH)-kernel/kernel-headers
-BOARD_BOOT_HEADER_VERSION := 4
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+TARGET_KERNEL_SOURCE := kernel/motorola/sm7435
+TARGET_KERNEL_CONFIG := \
+    gki_defconfig \
+    vendor/waipio_GKI.config \
+    vendor/ext_config/moto-waipio.config \
+    vendor/ext_config/moto-waipio-gki.config
+
+TARGET_KERNEL_EXT_MODULE_ROOT := kernel/motorola/sm7435-modules
+
+# Kernel Modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(TARGET_KERNEL_SOURCE)/modules.vendor_blocklist.msm.waipio
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.vendor_boot))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE)
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
+
+TARGET_KERNEL_EXT_MODULES := \
+    qcom/opensource/mmrm-driver \
+    qcom/opensource/audio-kernel \
+    qcom/opensource/camera-kernel \
+    qcom/opensource/cvp-kernel \
+    qcom/opensource/dataipa/drivers/platform/msm \
+    qcom/opensource/datarmnet/core \
+    qcom/opensource/datarmnet-ext/aps \
+    qcom/opensource/datarmnet-ext/offload \
+    qcom/opensource/datarmnet-ext/shs \
+    qcom/opensource/datarmnet-ext/perf \
+    qcom/opensource/datarmnet-ext/perf_tether \
+    qcom/opensource/datarmnet-ext/sch \
+    qcom/opensource/datarmnet-ext/wlan \
+    qcom/opensource/display-drivers/msm \
+    qcom/opensource/eva-kernel \
+    qcom/opensource/video-driver \
+    qcom/opensource/wlan/qcacld-3.0/.adrastea \
+    qcom/opensource/wlan/qcacld-3.0/.qca6750
+
+TARGET_KERNEL_EXT_MODULES += \
+    motorola/drivers/mmi_annotate \
+    motorola/drivers/mmi_info \
+    motorola/drivers/backlight/aw99703 \
+    motorola/drivers/backlight/ktd3136 \
+    motorola/drivers/power/bm_adsp_ulog \
+    motorola/drivers/power/mmi_charger \
+    motorola/drivers/power/qti_glink_charger \
+    motorola/drivers/power/qpnp_adaptive_charge \
+    motorola/drivers/power/bq27426_fg_mmi \
+    motorola/drivers/power/sgm4154x_charger_lite \
+    motorola/drivers/misc/utag \
+    motorola/drivers/misc/mmi_stow \
+    motorola/drivers/mmi_relay \
+    motorola/drivers/misc/mmi_sys_temp \
+    motorola/drivers/regulator/dio8015 \
+    motorola/drivers/regulator/wl2864c \
+    motorola/drivers/regulator/wl2866d \
+    motorola/drivers/regulator/slg5bm43670 \
+    motorola/drivers/sensors \
+    motorola/drivers/misc/awinic/sarsensor \
+    motorola/drivers/misc/stk501xx \
+    motorola/drivers/misc/sx937x \
+    motorola/drivers/input/touchscreen/touchscreen_mmi \
+    motorola/drivers/input/touchscreen/focaltech_0flash_v2_mmi \
+    motorola/drivers/input/touchscreen/ili9882_mmi \
+    motorola/drivers/input/touchscreen/goodix_berlin_mmi \
+    motorola/drivers/input/misc/anc_fps_mmi \
+    motorola/drivers/input/misc/fpc_fps_mmi \
+    motorola/drivers/input/misc/goodix_fod_mmi \
+    motorola/drivers/moto_netopt/con_dfpar \
+    motorola/drivers/nfc/st21nfc \
+    motorola/drivers/wlan_antenna \
+    motorola/drivers/regulator/dio8018 \
+    motorola/drivers/moto_f_mass_storage \
+    motorola/drivers/moto_f_usbnet \
+    motorola/drivers/moto_mm \
+    motorola/drivers/moto_swap \
+    motorola/drivers/ese/st54x \
+    motorola/drivers/wlan_elna \
+    motorola/drivers/input/misc/vl53L1_14_1_2 \
+    motorola/drivers/watchdogtest
+
+
+# Prebuilt Kernel Configs (Commented out for inline building)
+# BOARD_KERNEL_IMAGE_NAME := Image
+# BOARD_KERNEL_PAGESIZE := 4096
+# BOARD_KERNEL_SEPARATED_DTBO := true
+# BOARD_RAMDISK_USE_LZ4 := true
+# TARGET_KERNEL_NO_GCC := true
+# TARGET_KERNEL_SOURCE := $(DEVICE_PATH)-kernel/kernel-headers
+# BOARD_BOOT_HEADER_VERSION := 4
+# BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 # Prebuilt Kernel Modules
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)-kernel/vendor_ramdisk/*.ko)
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(patsubst %,$(DEVICE_PATH)-kernel/vendor_ramdisk/%,$(shell cat $(DEVICE_PATH)-kernel/vendor_ramdisk/modules.load))
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD  := $(patsubst %,$(DEVICE_PATH)-kernel/vendor_ramdisk/%,$(shell cat $(DEVICE_PATH)-kernel/vendor_ramdisk/modules.load.recovery))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)-kernel/vendor_ramdisk/modules.blocklist
-BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)-kernel/vendor_dlkm/*.ko)
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(patsubst %,$(DEVICE_PATH)-kernel/vendor_dlkm/%,$(shell cat $(DEVICE_PATH)-kernel/vendor_dlkm/modules.load))
-BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)-kernel/vendor_dlkm/modules.blocklist
+# BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)-kernel/vendor_ramdisk/*.ko)
+# BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(patsubst %,$(DEVICE_PATH)-kernel/vendor_ramdisk/%,$(shell cat $(DEVICE_PATH)-kernel/vendor_ramdisk/modules.load))
+# BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD  := $(patsubst %,$(DEVICE_PATH)-kernel/vendor_ramdisk/%,$(shell cat $(DEVICE_PATH)-kernel/vendor_ramdisk/modules.load.recovery))
+# BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)-kernel/vendor_ramdisk/modules.blocklist
+# BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)-kernel/vendor_dlkm/*.ko)
+# BOARD_VENDOR_KERNEL_MODULES_LOAD := $(patsubst %,$(DEVICE_PATH)-kernel/vendor_dlkm/%,$(shell cat $(DEVICE_PATH)-kernel/vendor_dlkm/modules.load))
+# BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)-kernel/vendor_dlkm/modules.blocklist
 
 # Prebuilt Kernel
-TARGET_FORCE_PREBUILT_KERNEL := true
-TARGET_NO_KERNEL_OVERRIDE := true
-TARGET_NO_KERNEL := false
-BOARD_KERNEL_BINARIES := kernel
-TARGET_KERNEL_VERSION := 5.10
+# TARGET_FORCE_PREBUILT_KERNEL := true
+# TARGET_NO_KERNEL_OVERRIDE := true
+# TARGET_NO_KERNEL := false
+# BOARD_KERNEL_BINARIES := kernel
+# TARGET_KERNEL_VERSION := 5.10
 
 # Paths to Prebuilt Images
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)-kernel/kernel
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)-kernel/dtb.img
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)-kernel/dtbo.img
+# TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)-kernel/kernel
+# TARGET_PREBUILT_DTB := $(DEVICE_PATH)-kernel/dtb.img
+# BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)-kernel/dtbo.img
 
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)-kernel/dtb.img:$(TARGET_COPY_OUT)/dtb.img \
-    $(DEVICE_PATH)-kernel/kernel:kernel
+# PRODUCT_COPY_FILES += \
+#     $(DEVICE_PATH)-kernel/dtb.img:$(TARGET_COPY_OUT)/dtb.img \
+#     $(DEVICE_PATH)-kernel/kernel:kernel
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
